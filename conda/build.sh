8db1d265f0e35bcce6a1d24b62c50ee66553bd69
Dinesh Kumar <dkumar@lbl.gov>
2018-02-08 14:54:12 -0800

    updated conda recipe

--- conda/build.sh
+++ conda/build.sh
@@ -1,14 +1,8 @@
-
-pip install fabio==0.4.0
-pip install pyFAI
+pip install fabio
+pip install PyFAI
 pip install qdarkstyle
 pip install nexusformat
-pip install pysftp
-pip install tifffile
-#pip install imageio
-#pip install qtconsole
-#pip install vispy
-#pip install scikit-image
+pip install scikit-image
 
 pip install . --no-deps
 
